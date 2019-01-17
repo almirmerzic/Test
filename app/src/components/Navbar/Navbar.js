e517ef6a0f2079ffb0834c10797d033d2593e98e
almirmerzic <33768032+almirmerzic@users.noreply.github.com>
2019-01-17 14:48:08 +0100

    Navbar and logo
    Navbar changed and logo added.

--- app/src/components/Navbar/Navbar.js
+++ app/src/components/Navbar/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import logo from './logo.jpg';
+import { Link } from 'react-router-dom';
 
-class Navbar extends React.Component{
-    render(){
-        return(
-        <div>Navbar Component</div>
-        );
-    }
-}
-
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div>
+                <nav class="navbar navbar-light bg-light">
+                    <p class="navbar-brand" href="#">
+                    <Link to="/"><img src={logo} className="App-logo" alt="logo" /></Link> 
+                    </p>
+                 </nav>
+        </div>
+                );
+            }
+        }
+        
 export default Navbar;
