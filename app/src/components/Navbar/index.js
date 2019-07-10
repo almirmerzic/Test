283ed158fd17f880002eb141f08c7e4fd7fbec1e
almirmerzic <33768032+almirmerzic@users.noreply.github.com>
2019-07-11 01:58:18 +0200

    Update index.js

--- app/src/components/Navbar/index.js
+++ app/src/components/Navbar/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import logo from './logo.jpg';
 
 const Navbar = () => (
     <div className="text-center">
         <nav className="navbar navbar-expand-lg">
-            <img src={logo} className="App-logo" alt="logo" />
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
             </div>
         </nav>
